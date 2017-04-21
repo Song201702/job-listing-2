4ed823dd20a3aad9a2201b40ef3ea1cb545588d0
Chen Song <songminmin999@gmail.com>
2017-04-21 15:57:53 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -32,7 +32,7 @@ before_action :authenticate_user!, only:[:new, :create, :eidt, :update, :destroy
     if @job.update(job_params)
       redirect_to jobs_path
     else
-      reder :edit
+      render :edit
     end
   end
 
@@ -46,6 +46,6 @@ before_action :authenticate_user!, only:[:new, :create, :eidt, :update, :destroy
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
