f236d7e112cda580b56dc5ecca9a68435761115e
Chen Song <songminmin999@gmail.com>
2017-04-28 22:54:25 +0800

    search function1

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,7 @@
 class JobsController < ApplicationController
 before_action :authenticate_user!, only:[:new, :create, :eidt, :update, :destroy]
+before_action :validate_search_key, only: [:search]
+
   def index
     @jobs = case params[:order]
     when 'by_lower_bound'
@@ -54,6 +56,24 @@ before_action :authenticate_user!, only:[:new, :create, :eidt, :update, :destroy
       redirect_to jobs_path
   end
 
+  def search
+    if @query_string.present?
+      search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
 
   private
 
